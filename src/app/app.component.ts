1b1125d55b96b3275cb9c98c4878c720ff794d01
Howard.Zuo <leftstick@qq.com>
2020-06-08 12:42:07 +0800

    fix: #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,43 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 import {
   ControlAnchor,
   MapOptions,
   NavigationControlOptions,
   NavigationControlType,
-  Point
-} from 'angular2-baidu-map'
+  Point,
+} from 'angular2-baidu-map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  styleUrls: ['./app.component.styl'],
 })
 export class AppComponent {
-  title = 'angular5 Baidu-Map example'
+  title = 'angular5 Baidu-Map example';
 
-  options: MapOptions
-  point: Point
-  navOptions: NavigationControlOptions
+  options: MapOptions;
+  point: Point;
+  navOptions: NavigationControlOptions;
 
   constructor() {
     this.options = {
       centerAndZoom: {
         lat: 39.920116,
         lng: 116.403703,
-        zoom: 16
+        zoom: 16,
       },
-      enableKeyboard: true
-    }
+      enableKeyboard: true,
+    };
 
     this.point = {
       lat: 39.920109,
-      lng: 116.403705
-    }
+      lng: 116.403705,
+    };
 
     this.navOptions = {
       anchor: ControlAnchor.BMAP_ANCHOR_TOP_RIGHT,
-      type: NavigationControlType.BMAP_NAVIGATION_CONTROL_PAN
-    }
+      type: NavigationControlType.BMAP_NAVIGATION_CONTROL_PAN,
+    };
   }
 }
